4ec0766bd35dad07281410e2910a8005d157b030
Mike Simpson <mike_simpson_159@outlook.com>
2024-06-02 10:47:45 +0100

    Requests wasn't working, so I have re-written it to use sockets and have more acurate timing

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import app
 import requests
 import time
+import usocket as socket
 from events.input import Buttons, BUTTON_TYPES
 from app_components import clear_background
 
 class SpeedTest(app.App):
     def __init__(self):
         self.button_states = Buttons(self)
-        url = "http://ipv4.download.thinkbroadband.com/5GB.zip"
+        url = "http://ash-speed.hetzner.com/100MB.bin"
         self.dl_speed = "Downloading..."
         self.dl_speed = download_and_discard(url)
 
@@ -17,7 +18,7 @@ class SpeedTest(app.App):
             # Calling clear() ensures the next time you open the app, it stays open.
             # Without it the app would close again immediately.
             self.button_states.clear()
-            self.minimise()
+            self.close()
 
     def draw(self, ctx):
         clear_background(ctx)
@@ -27,24 +28,65 @@ class SpeedTest(app.App):
         ctx.restore()
 
 def download_and_discard(url):
-    print("starting download")
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        total_bytes = 0
-        start_time = time.time()
-        end_time = time.time()
-        for chunk in r.iter_content(chunk_size=8192): # Probably about as much memory as we want to buffer
-            if chunk:
-                total_bytes += len(chunk)
-            if total_bytes % 81920 == 0:
-                end_time = time.time()
-                if end_time - start_time > 5:
-                    break
-        end_time = time.time()
-        duration = end_time - start_time
-        speed = total_bytes / duration / 1024  # Speed in KB/s
-        print(duration)
-        print(speed)
-    return "{:,.3f}".format(speed) + " KB/s"
+    print("Starting download")
+    start_time = time.ticks_ms()
+    total_bytes = 0
+
+    try:
+        # Parse the URL
+        if not url.startswith('http://'):
+            raise ValueError("Only HTTP URLs are supported")
+
+        url = url[7:]  # Remove 'http://'
+        host, _, path = url.partition('/')
+        path = '/' + path
+
+        # Resolve the IP address of the host
+        addr_info = socket.getaddrinfo(host, 80)
+        if not addr_info:
+            raise Exception("Unable to resolve host")
+        addr = addr_info[0][-1]
+
+        # Create a socket and connect
+        s = socket.socket()
+        s.connect(addr)
+
+        # Send the HTTP request
+        request = "GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n".format(path, host)
+        s.send(request.encode())
+
+        # Read the response headers
+        while True:
+            line = s.readline()
+            print(line)  # Debug: Print header lines
+            if line == b"\r\n":
+                break
+
+        # Read the response content in chunks
+        while True:
+            chunk = s.recv(16384)  # Use recv to handle chunks
+            if not chunk:
+                break
+            total_bytes += len(chunk)
+            #print("Read chunk of size:", len(chunk))  # Debug: Print chunk size
+            end_time = time.ticks_ms()
+            if time.ticks_diff(end_time, start_time) > 5000: 
+                break
+
+    except Exception as e:
+        print("An error occurred:", e)
+    
+    finally:
+        s.close()
+
+    print("Done download")
+    end_time = time.time()
+    duration = time.ticks_diff(time.ticks_ms(), start_time) / 1000  
+    if duration is 0:
+        duration = 0.001
+    speed = total_bytes / duration / 1000  # Speed in KB/s
+    print("Duration:", duration)
+    print("Speed:", speed)
+    return "{:,.3f}".format(speed) + " KB/s\n""{:,.3f}".format(speed*8) + " Kb/s"
 
 __app_export__ = SpeedTest
